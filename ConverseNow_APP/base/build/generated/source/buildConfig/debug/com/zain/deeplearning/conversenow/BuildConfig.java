5f77959a8e1fbe46961261ca9a2812402ff50e46
Akhil Singh <40403909+theDrake1010@users.noreply.github.com>
2020-01-03 20:39:05 +0530

    added the app files
    had forgot to push this

---
+++ ConverseNow_APP/base/build/generated/source/buildConfig/debug/com/zain/deeplearning/conversenow/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zain.deeplearning.conversenow;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.zain.deeplearning.conversenow";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.zain.deeplearning.conversenow";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ ConverseNow_APP/conv_now/build/generated/source/buildConfig/debug/com/zain/deeplearning/conversenow/conv_now/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zain.deeplearning.conversenow.conv_now;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.zain.deeplearning.conversenow.conv_now";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.zain.deeplearning.conversenow.conv_now";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
